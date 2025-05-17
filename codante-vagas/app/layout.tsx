905534291da27bb85395069e7be7a322a7d3e1d3
Deise Santana <dsds3@aluno.ifnmg.edu,br>
2025-05-17 19:30:50 -0300

    next/font

--- codante-vagas/app/layout.tsx
+++ codante-vagas/app/layout.tsx
@@ -2,6 +2,13 @@ import Navbar from "@/components/layout/navbar";
 import "./globals.css";
 import Footer from "@/components/layout/footer";
 import { Metadata } from "next";
+import { Maven_Pro } from "next/font/google";
+
+const mavenPro = Maven_Pro({
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-maven-pro",
+});
 
 export const metadata: Metadata = {
   title: "CodanteVagas",
@@ -15,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={mavenPro.variable}>
       <body>
         <div className="flex min-h-screen flex-col">
           <Navbar />
