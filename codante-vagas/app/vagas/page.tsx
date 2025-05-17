9a971f343bd86e518f2897fccd492da1cd4418a5
Deise Santana <dsds3@aluno.ifnmg.edu,br>
2025-05-17 19:09:40 -0300

    vagas e cadastrar vagas

--- codante-vagas/app/vagas/page.tsx
+++ codante-vagas/app/vagas/page.tsx
@@ -1,7 +1,22 @@
+import JobItem from "@/components/cards/job-item";
+
+const job = {
+  id: "1",
+  title: "Designer",
+  company: "Google Brasil",
+  city: "São Paulo",
+  salary: 10000,
+};
+
 export default function Vagas() {
   return (
-    <div>
-      <h1>Listagem de Vagas</h1>
-    </div>
+    <main className="py-10">
+      <h2 className="font-display mb-12 text-2xl font-bold">Todas as Vagas</h2>
+      <div className="space-y-8">
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+      </div>
+    </main>
   );
 }
