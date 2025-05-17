ca5ac37499d1d72e93235988e4225d1ae227c1d7
Deise Santana <dsds3@aluno.ifnmg.edu,br>
2025-05-17 14:37:20 -0300

    criado e estilizado pagina 404 error

--- codante-vagas/app/not-found.tsx
+++ codante-vagas/app/not-found.tsx
@@ -1,7 +1,26 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Ops, nada encontrado aqui!</h1>
-    </div>
+    <main className="mx-auto max-w-5xl py-20 text-center">
+      <h2 className="font-display text-4xl font-black">
+        Nada encontrado aqui...
+      </h2>
+      <img
+        className="mx-auto w-sm"
+        src="/cone.png"
+        alt="ilustração de um barco"
+      />
+      <p className="text-3xl font-extralight text-gray-400">
+        Não foi possível encontrar a página solicitada.
+      </p>
+      <a
+        className={cn("mt-10 block", buttonVariants({ variant: "default" }))}
+        href="/"
+      >
+        Voltar à Home
+      </a>
+    </main>
   );
 }
